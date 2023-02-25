1819ff5256d066caa97b376726ded1c3580f82bc
mulkiw <kefr.left@gmail.com>
2023-02-25 14:14:28 +0300

    Windows_Version_Checker
    Required Python

---
+++ Run.bat
@@ -0,0 +1,10 @@
+@echo off
+title Windows.Version.Checker
+echo (Mulkiw Corporation 2023)
+echo.
+echo ___Windows.Version.Checker___
+echo _____________________________
+echo.
+py VersionChecker.py
+echo.
+pause > nul
